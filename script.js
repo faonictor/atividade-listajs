c3a8c631c846c54868a3289eb4a85e5f49c07140
faonictor <jnictoraraujo@gmail.com>
2024-04-27 16:08:29 -0300

    add-contador-de-remocao

--- script.js
+++ script.js
@@ -14,26 +14,26 @@ function defaultCorDiv(){
 }
 
 //-------------> Q3
+var contRemove = 1
 function addParagrafo(main_container) {
 
     const container = document.getElementById(main_container);
-    const id = 'p-' + (container.children.length + 1); //gerador de id
+    const id = 'p-' + (container.children.length + contRemove); //gerador de id
 
-    const novoElementoHTML = `<p class="p-paragrafo" id="${id}">
-                                Parágrafo ${container.children.length + 1}
+    const elementoP = `<p class="p-paragrafo" id="${id}">
+                                Parágrafo ${container.children.length + contRemove}
                                 <button class="btn" onclick="removerParagrafo('${id}')">Remover</button>
                               </p>`;
 
-                              
-
     const novoElemento = document.createElement('div');
-    novoElemento.innerHTML = novoElementoHTML;
+    novoElemento.innerHTML = elementoP;
     container.appendChild(novoElemento.firstChild)
 }
 
 //-------------> Q4
 function removerParagrafo(elementoId) {
     document.getElementById(elementoId).remove();
+    contRemove++
 }
 
 
